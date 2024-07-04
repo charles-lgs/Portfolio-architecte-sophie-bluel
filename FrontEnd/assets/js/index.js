11737d34de1891a1ffae0d1311cbb844a80687fe
Charles Langlois <charles.langlois.dev@gmail.com>
2024-07-04 16:08:02 +0200

    Filtre-dynamique-par-catégories

--- FrontEnd/assets/js/index.js
+++ FrontEnd/assets/js/index.js
@@ -8,24 +8,31 @@ async function getWorks() {
 }
 
 //////////// Display works on the DOM ////////////
+
+// Divide the array into work //
 async function displayWorks() {
   const dataWorks = await getWorks();
 
   dataWorks.forEach((work) => {
-    const figure = document.createElement("figure");
-    const image = document.createElement("img");
-    const figcaption = document.createElement("figcaption");
-
-    // Display images and titles
-    image.src = work.imageUrl;
-    figcaption.textContent = work.title;
-
-    figure.appendChild(image);
-    figure.appendChild(figcaption);
-    gallery.appendChild(figure);
+    createWorks(work);
   });
 }
 displayWorks();
+
+// Creating elements in the DOM //
+function createWorks(work) {
+  const figure = document.createElement("figure");
+  const image = document.createElement("img");
+  const figcaption = document.createElement("figcaption");
+
+  // Display images and titles
+  image.src = work.imageUrl;
+  figcaption.textContent = work.title;
+
+  figure.appendChild(image);
+  figure.appendChild(figcaption);
+  gallery.appendChild(figure);
+}
 
 //////////// Function to retrieve categories ////////////
 async function getCategories() {
@@ -40,6 +47,7 @@ async function displayCategories() {
   categories.forEach((category) => {
     const button = document.createElement("button");
 
+    // Show button content and add CSS styling
     button.textContent = category.name;
     button.id = category.id;
     button.classList.add("btn-filter");
@@ -48,3 +56,28 @@ async function displayCategories() {
   });
 }
 displayCategories();
+
+//////////// Filter works by category ////////////
+async function filterCategory() {
+  const works = await getWorks();
+  const buttons = document.querySelectorAll(".container-btn-filter button");
+
+  buttons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+      buttonId = e.target.id;
+      gallery.innerHTML = "";
+
+      if (buttonId !== "0") {
+        const worksFilterCategory = works.filter((work) => {
+          return work.categoryId == buttonId;
+        });
+        worksFilterCategory.forEach((work) => {
+          createWorks(work);
+        });
+      } else {
+        displayWorks();
+      }
+    });
+  });
+}
+filterCategory();
