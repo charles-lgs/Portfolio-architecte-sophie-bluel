e2eace5d924678873bbc47bd7082942bb3c3c80f
Charles Langlois <charles.langlois.dev@gmail.com>
2024-07-04 14:34:24 +0200

    Affichage-des-boutons-sur-le-DOM

--- FrontEnd/assets/js/index.js
+++ FrontEnd/assets/js/index.js
@@ -1,4 +1,5 @@
 const gallery = document.querySelector(".gallery");
+const containerButton = document.querySelector(".container-btn-filter");
 
 //////////// Function to retrieve works ////////////
 async function getWorks() {
@@ -6,7 +7,7 @@ async function getWorks() {
   return await response.json();
 }
 
-//////////// Display function on the DOM ////////////
+//////////// Display works on the DOM ////////////
 async function displayWorks() {
   const dataWorks = await getWorks();
 
@@ -31,4 +32,19 @@ async function getCategories() {
   const response = await fetch("http://localhost:5678/api/categories");
   return await response.json();
 }
-getCategories();
+
+//////////// Display categories button on the DOM ////////////
+async function displayCategories() {
+  const categories = await getCategories();
+
+  categories.forEach((category) => {
+    const button = document.createElement("button");
+
+    button.textContent = category.name;
+    button.id = category.id;
+    button.classList.add("btn-filter");
+
+    containerButton.appendChild(button);
+  });
+}
+displayCategories();
