ebfb67b35da4b1a60d20de1664a105ebd7694964
Charles Langlois <charles.langlois.dev@gmail.com>
2024-07-17 10:08:02 +0200

    Fix/suppression-fichier-modale.js

--- FrontEnd/assets/js/index.js
+++ FrontEnd/assets/js/index.js
@@ -141,3 +141,25 @@ async function filterCategory() {
     });
   });
 }
+
+// \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\ //
+//////////////////// Modale part //////////////////
+// \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\ //
+
+const linkProject = document.querySelector(".link-project");
+const containerModale = document.querySelector(".container-modal");
+const xMark = document.querySelector(".fa-xmark");
+
+linkProject.addEventListener("click", () => {
+  containerModale.style.display = "flex";
+});
+
+xMark.addEventListener("click", () => {
+  containerModale.style.display = "none";
+});
+
+containerModale.addEventListener("click", (e) => {
+  if (e.target.classList.value == "container-modal") {
+    containerModale.style.display = "none";
+  }
+});
